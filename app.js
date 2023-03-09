32ad6de0ed28d5fa146b9eb5ccb1f324262e7679
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2023-03-09 16:54:40 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const favicon = require('serve-favicon')  //Middleware pour mettre la favicon en cache  
+const morgan = require('morgan') //Middleware pour afficher le type de la requête entrante , son URL, ainsi que le code de statut
+const bodyParser = require('body-parser') // Node.js body parsing middleware 
+
+const app = express()
+const port = 5000
+
+app
+.use(favicon(__dirname + '/favicon.ico'))
+.use(morgan('dev'))
+.use(bodyParser.json())
+
+app.get('/', (req,res) => 
+ res.send("Hello traveler!")
+)
+
+app.listen(port, () => console.log(`Notre application est démarrée sur : http://localhost:${port}`))
